d20065f1a0d02efe917b7093d9b266983468d7fb
Moselikk <moselikk@gmail.com>
2023-02-22 13:38:26 +0800

    fix: husky

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,6 +1,8 @@
 <template>
   <h1 style="text-align: center">About</h1>
   <i-ic-baseline-60fps />
+  <i-ic-baseline-60fps />
+  <i-ic-baseline-60fps />
 </template>
 
 <script setup lang="ts"></script>
